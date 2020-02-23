e36c0df629dc682e11c5cda66dc4e54a70ae476b
Kim Duclos <kimduclos@gmail.com>
2020-02-22 18:46:49 -0700

    updated favicon and tab title

--- src/TitleSection/TitleSection.jsx
+++ src/TitleSection/TitleSection.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import "./TitleSection.scss";
+import Link from "@material-ui/core/Link";
 import Button from "@material-ui/core/Button";
 
 const TitleSection = () => {
-    return (
-        <>
-            <div className="title">
-                Storm Peak Engineering
-            </div>
-            <div className="description">
-                Description of your business goes here
-            </div>
-            <div className="socialButtons">
-                <Button variant="contained">LinkedIn</Button>
-                <Button variant="contained">GitHub</Button>
-                <Button variant="contained">Email</Button>
-            </div>
-
-        </>    
-    );
-}
+  return (
+    <>
+      <div className="title">Storm Peak Engineering</div>
+      <div className="description">Description of your business goes here</div>
+      <div className="socialButtons">
+        <Link
+          href="https://www.linkedin.com/in/sean-donohue-7340842a/"
+          target="blank"
+        >
+          <Button variant="contained">LinkedIn</Button>
+        </Link>
+        <Link href="https://github.com/skrawn" target="blank">
+          <Button variant="contained">GitHub</Button>
+        </Link>
+        <Link to="mailto:sdonohue331@gmail.com?subject=Portfolio Contact" target="blank">
+          <Button variant="contained">Email</Button>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default TitleSection;
