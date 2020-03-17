5869664ac9feb69e89fff01dd261daa0cf656a4b
Kim Duclos <kimduclos@gmail.com>
2020-03-17 11:21:16 -0600

    panel icons updated

--- src/Services/ServicePanel.jsx
+++ src/Services/ServicePanel.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./ServicePanel.scss";
 
-const ServicePanel = (props) => {
+const ServicePanel = props => {
   return (
     <div className="grow" style={{ backgroundColor: "#EBF095" }}>
       <h2>{props.serviceTitle}</h2>
-      <p>
-        {props.serviceDescription}
-      </p>
+      <p>{props.serviceDescription}</p>
+      <img
+        src={props.serviceIcon1}
+        alt="service icon"
+        className="softwareIcon1"
+      />
+      <img
+        src={props.serviceIcon2}
+        alt="service icon"
+        className="softwareIcon2"
+      />
+      <img
+        src={props.serviceIcon3}
+        alt="service icon"
+        className="softwareIcon3"
+      />
+      <img
+        src={props.serviceIcon4}
+        alt="service icon"
+        className="softwareIcon4"
+      />
     </div>
   );
 };
--- src/Title/Title.jsx
+++ src/Title/Title.jsx
@@ -3,8 +3,8 @@ import "./Title.scss";
 // import Link from "@material-ui/core/Link";
 // import Button from "@material-ui/core/Button";
 import SocialButton from "./SocialButton.jsx";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import GitHubIcon from "@material-ui/icons/GitHub";
+// import LinkedInIcon from "@material-ui/icons/LinkedIn";
+// import GitHubIcon from "@material-ui/icons/GitHub";
 import EmailIcon from "@material-ui/icons/Email";
 
 const TitleSection = () => {
@@ -14,7 +14,7 @@ const TitleSection = () => {
       <div className="title">Storm Peak Engineering</div>
       <div className="description">Description of your business goes here</div>
       <div className="socialButtons">
-        <SocialButton
+        {/* <SocialButton
           socialLink="https://www.linkedin.com/in/sean-donohue-7340842a/"
           socialName="LinkedIN"
           socialIcon={<LinkedInIcon />}
@@ -23,7 +23,7 @@ const TitleSection = () => {
           socialLink="https://github.com/skrawn"
           socialName="GitHub"
           socialIcon={<GitHubIcon />}
-        />
+        /> */}
         <SocialButton
           socialLink="mailto:sdonohue331@gmail.com?subject=Hello from your portfolio page"
           socialName="Email"
